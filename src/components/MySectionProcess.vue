f49713d6e7c773b9d0fc2d074d2bbb14ab019c01
Marco Braccini <marco.braccini@live.it>
2022-09-06 23:26:26 +0200

    Add section 2process" with css

--- src/components/MySectionProcess.vue
+++ src/components/MySectionProcess.vue
@@ -1,31 +1,23 @@
 <template>
-    <section>
-        <div class="container">
-            <div class="flex just-ctr">
-                <div class="process">
-
+    <section class="py-120 txt-ctr">
+        <div>
+            <div class="process">
+                <div class="container">
+                    <div class="color-brand-dark txt-up txt-bold">How it works in pratice</div>
+                    <h2 class="mt-20"><span class="band-tag-back color-brand-dark">Process</span> Management</h2>
+                    <p class="mt-20">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto laudantium laborum, mollitia ullam, explicabo recusandae commodi iste id expedita aut sequi necessitatibus.</p>
                 </div>
             </div>
 
-            <div class="flex wrap">
-                <!-- VUE CYCLE -->
-                <div class="process-card">
+            <div class="process-card-container mt-40">
+                <div class="process-card-fx"></div>
 
+                <div class="container flex wrap just-sp-bw">
+                    <div class="process-card" v-for="(process, processIndex) in processCard" :key="processIndex">
+                    <span class="circle-num">{{process.number}}</span>
+                    <h4 class="mt-20">{{process.title}}</h4>
+                    <p class="mt-20">{{process.overview}}</p>
                 </div>
-                <div class="process-card">
-
-                </div>
-                <div class="process-card">
-
-                </div>
-                <div class="process-card">
-
-                </div>
-                <div class="process-card">
-
-                </div>
-                <div class="process-card">
-
                 </div>
             </div>
         </div>
@@ -34,7 +26,37 @@
 
 <script>
 export default {
-    name: 'MySectionProcess'
+    name: 'MySectionProcess',
+    data() {
+        return {
+            processCard: [
+                {
+                    number: '01',
+                    title: 'Collection of information',
+                    overview: 'Lorem ipsum dolor sit amet consectetur'
+                },{
+                    number: '02',
+                    title: 'Strategic planning',
+                    overview: 'Lorem ipsum dolor sit amet consectetur'
+                },
+                {
+                    number: '03',
+                    title: 'Assignment of responsibilities',
+                    overview: 'Lorem ipsum dolor sit amet consectetur'
+                },
+                {
+                    number: '04',
+                    title: 'Formatting process',
+                    overview: 'Lorem ipsum dolor sit amet consectetur'
+                },
+                {
+                    number: '05',
+                    title: 'Continuity formalization',
+                    overview: 'Lorem ipsum dolor sit amet consectetur'
+                }
+            ]
+        }
+    }
 }
 </script>
 
@@ -42,10 +64,29 @@ export default {
 @import '@/styles/vars.scss';
 
     .process {
-        width: 60%;
+        .container {
+            width: 50%;
+        }
+    }
+
+    .process-card-container {
+        position: relative;
+    }
+
+    .process-card-fx {
+        height: 4px;
+        width: 100%;
+        background-color:rgba(4, 131, 131, 0.2);
+        position: absolute;
+        top: 18px;
+        left: 50%;
+        transform: translateX(-50%);
+        z-index: 1;
     }
 
     .process-card {
-        width: calc(100% / 6);
+        width: calc(94% / 5);
+        position: relative;
+        z-index: 2;
     }
 </style>
